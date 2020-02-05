80cede9946edc16c2545a79b525d0db55844d0be
Rachit-codifier <44037733+rexj8@users.noreply.github.com>
2020-02-05 19:55:45 +0530

    displaying a image on button

---
+++ IMG_on_BTN.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class ButtonExample{    
+
+  ButtonExample()
+  {    
+    JFrame f=new JFrame("Button Example");            
+    JButton b=new JButton(new ImageIcon("C:\\pic.png"));    
+    b.setBounds(100,100,100, 40);    
+    f.add(b);    
+    f.setSize(300,400);    
+    f.setLayout(null);    
+    f.setVisible(true);    
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+   } 
+   
+   
+public static void main(String[] args) {    
+    new ButtonExample();    
+  }    
+}  
