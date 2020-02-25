0074bdbc33f6c8a03e903b2360aebf6d33f8fddd
Rachit-codifier <44037733+rexj8@users.noreply.github.com>
2020-02-25 22:48:40 +0530

    Create BufferedWriter.java

---
+++ File_Handling/BufferedWriter.java
@@ -0,0 +1,16 @@
+import java.io.*;  
+
+public class BufferedWriterExample {  
+
+public static void main(String[] args) throws Exception {
+
+    FileWriter writer = new FileWriter("D:\\testout.txt");  
+    
+    BufferedWriter buffer = new BufferedWriter(writer);  
+    
+    buffer.write("Welcome to java.");  
+    buffer.close();  
+    
+    System.out.println("Success");  
+    }  
+} 
