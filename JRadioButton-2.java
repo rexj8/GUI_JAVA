aa36f6805c9b23b27a9c8e98599b34b27e2407f1
Rachit-codifier <44037733+rexj8@users.noreply.github.com>
2020-03-10 23:45:17 +0530

    Update JRadioButton-2.java

--- JRadioButton-2.java
+++ JRadioButton-2.java
@@ -3,60 +3,60 @@ import java.awt.event.*;
 
 class Demo extends JFrame {
 
-    // Declaration of object of JRadioButton class.
+    // Declaration of object of JRadioButton class
     JRadioButton jRadioButton1;
 
-    // Declaration of object of JRadioButton class.
+    // Declaration of object of JRadioButton class
     JRadioButton jRadioButton2;
 
-    // Declaration of object of JButton class.
+    // Declaration of object of JButton class
     JButton jButton;
 
-    // Declaration of object of ButtonGroup class.
+    // Declaration of object of ButtonGroup class
     ButtonGroup G1;
 
-    // Declaration of object of JLabel class.
+    // Declaration of object of JLabel class
     JLabel L1;
 
-    // Constructor of Demo class.
+    // Constructor of Demo class
     public Demo()
     {
 
-        // Setting layout as null of JFrame.
+        // Setting layout as null of JFrame
         this.setLayout(null);
 
-        // Initialization of object of "JRadioButton" class.
+        // Initialization of object of "JRadioButton" class
         jRadioButton1 = new JRadioButton();
 
-        // Initialization of object of "JRadioButton" class.
+        // Initialization of object of "JRadioButton" class
         jRadioButton2 = new JRadioButton();
 
-        // Initialization of object of "JButton" class.
+        // Initialization of object of "JButton" class
         jButton = new JButton("Click");
 
-        // Initialization of object of "ButtonGroup" class.
+        // Initialization of object of "ButtonGroup" class
         G1 = new ButtonGroup();
 
-        // Initialization of object of " JLabel" class.
+        // Initialization of object of " JLabel" class
         L1 = new JLabel("Qualification");
 
-        // setText(...) function is used to set text of radio button.
-        // Setting text of "jRadioButton2".
+        // setText(...) function is used to set text of radio button
+        // Setting text of "jRadioButton2"
         jRadioButton1.setText("Under-Graduate");
 
-        // Setting text of "jRadioButton4".
+        // Setting text of "jRadioButton4"
         jRadioButton2.setText("Graduate");
 
-        // Setting Bounds of "jRadioButton2".
+        // Setting Bounds of "jRadioButton2"
         jRadioButton1.setBounds(120, 30, 120, 50);
 
-        // Setting Bounds of "jRadioButton4".
+        // Setting Bounds of "jRadioButton4"
         jRadioButton2.setBounds(250, 30, 80, 50);
 
-        // Setting Bounds of "jButton".
+        // Setting Bounds of "jButton"
         jButton.setBounds(125, 90, 80, 30);
 
-        // Setting Bounds of JLabel "L2".
+        // Setting Bounds of JLabel "L2"
         L1.setBounds(20, 30, 150, 50);
 
         // "this" keyword in java refers to current object.
